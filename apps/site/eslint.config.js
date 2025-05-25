0b7ec2f17aff8d44504955378e6d369a22c17f39
Hak Lee <me@haklee.me>
2025-05-25 21:36:50 +0900

    refactor: rename "web" app to "site"

--- apps/site/eslint.config.js
+++ apps/site/eslint.config.js
@@ -1,4 +1,4 @@
-import {config} from "@coderplace/eslint-config/next-js";
+import { config } from '@coderplace/eslint-config/next-js';
 
 /** @type {import("eslint").Linter.Config} */
 export default config;
